c37e803ceb4db1d69108fa7db8fd1ac901a50017
Schengen <72796090+AlexeyTuralysov@users.noreply.github.com>
2022-01-19 08:21:40 +0900

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,15 @@
+
+  VANTA.TRUNK({
+    el: "#intro",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x1e1e1e,
+    backgroundColor: 0xffffff,
+    spacing: 6.50,
+    chaos: 10.00
+  })
---
+++ js/font_wrap.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $("label#div1").click(function() {
+    
+      $("div#openings_wrapper1").slideToggle('slow');
+     
+    });
+    });
+    
+    
+    $(document).ready(function() {
+    $("label#div2").click(function() {
+    
+      $("div#openings_wrapper2").slideToggle('slow');
+    });
+    });
+    
+    
+    $(document).ready(function() {
+    $("label#div3").click(function() {
+    
+      $("div#openings_wrapper3").slideToggle('slow');
+    });
+    });
+    
+    
+    $(document).ready(function() {
+      $("label#div4").click(function() {
+      
+        $("div#openings_wrapper2").slideToggle('slow');
+      });
+      });
